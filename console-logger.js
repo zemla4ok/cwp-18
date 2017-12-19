cb89f0ec7dc9ec84ec3c3e9b6242d9820610cccc
zemla4ok <dima-vital@live.com>
2017-12-19 22:06:15 +0300

    task 2

---
+++ console-logger.js
@@ -0,0 +1,36 @@
+const Logger = require('./logger');
+const moment = require('moment');
+
+class ConsoleLogger extends Logger{
+    constructor(prefix = "prefix", defaultLevel = "LOG", dateFormat = "dddd, MMMM Do YYYY, h:mm:ss a") {
+        super(prefix, defaultLevel, dateFormat);
+    }
+
+    format(message, level = this.defaultLevel) {
+        return `${moment().format(this.dateFormat)} | ${this.prefix} | ${message}\n`;
+    }
+
+    log(message, level = this.defaultLevel) {
+        switch (level) {
+            case "LOG": {
+                console.log(this.format(message, level));
+                break;
+            }
+            case "INFO": {
+                console.info(this.format(message, level));
+                break;
+            }
+            case "WARN": {
+                console.warn(this.format(message, level));
+                break;
+            }
+            case "ERROR": {
+                console.error(this.format(message, level));
+                break;
+            }
+            default: {
+                console.log(this.format(message, this.defaultLevel));
+            }
+        }
+    }
+}
