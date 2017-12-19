a01bd9e66be7ff6cf9bd017615ff415e4415d55e
zemla4ok <dima-vital@live.com>
2017-12-19 22:09:17 +0300

    task 3

---
+++ file-logger.js
@@ -0,0 +1,34 @@
+const Logger = require('./logger');
+const fs = require('fs');
+
+class FileLogger extends Logger{
+    constructor(file = "default.txt", prefix = "prefix", defaultLevel = "LOG", dateFormat = "dddd, MMMM Do YYYY, h:mm:ss a") {
+        super(prefix, defaultLevel, dateFormat);
+        this.file = file;
+    }
+
+    log(message, level = this.defaultLevel) {
+        return new Promise((resolve, reject) => {
+            if (typeof(this.file) === 'string' || this.file instanceof String) {
+                fs.appendFile(this.file, this.format(message, level), (err) => {
+                    if (err) {
+                        console.error(err);
+                        reject(false);
+                    }
+                    else resolve(true);
+                });
+            }
+            else {
+                this.file.write(this.format(message, level), (err) => {
+                    if (err) {
+                        console.error(err);
+                        reject(false);
+                    }
+                    else resolve(true);
+                });
+            }
+        });
+    }
+}
+
+module.exports = FileLogger;
