380eeea7a655861ecee6043973bde576e4ca5ed5
zemla4ok <dima-vital@live.com>
2017-12-19 22:16:20 +0300

    task 4

---
+++ deferred-file-logger.js
@@ -0,0 +1,44 @@
+const FileLogger = require('./file-logger');
+const fs = require('fs');
+
+class DeferredFileLogger extends FileLogger{
+    constructor(file = "default.txt", queueLength = 3, prefix = "prefix", defaultLevel = "LOG", dateFormat = "dddd, MMMM Do YYYY, h:mm:ss a") {
+        super(file, prefix, defaultLevel, dateFormat);
+        this.queueLength = queueLength;
+    }
+
+    log(message, level = this.defaultLevel) {
+        queue.push(this.format(message));
+        return new Promise((resolve, reject) => {
+            if (queue.length === this.queueLength) {
+                if (typeof(this.file) === 'string' || this.file instanceof String) {
+                    fs.appendFile(this.file, queue.join(''), (err) => {
+                        if (err) {
+                            console.error(err);
+                            reject(false);
+                        }
+                        else {
+                            queue = [];
+                            resolve(true);
+                        }
+                    });
+                }
+                else {
+                    this.file.write(queue.join(''), (err) => {
+                        if (err) {
+                            console.error(err);
+                            reject(false);
+                        }
+                        else {
+                            queue = [];
+                            resolve(true);
+                        }
+                    });
+                }
+            }
+            else resolve(true);
+        });
+    }
+}
+
+module.exports = DeferredFileLogger;
